074e64c22aa80d2f20d29ca84d9af790a48cc587
caglarptimum7 <caglar@optimum7.com>
2020-06-12 16:11:51 +0300

    netlify

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
 import 'normalize.css'
 import './styles/style.scss'
 import axios from 'axios'
-let settings = {
-  headers: {
-    Accept: 'application/json',
-    Authorization: 'Bearer wchufrIMyAPbjYd4ntbiDSK+WWaP4/AYZfVksIvfQrI=',
-    SecureURL: 'https://7206341744.3dcart.net/',
-    Token: 'wchufrIMyAPbjYd4ntbiDSK+WWaP4/AYZfVksIvfQrI='
-  }
-}
-window.onload = () => {
-  console.log('loaded')
+// let settings = {
+//     method: 'GET',
+//     mode: 'no-cors',
+//     headers: {
+//         "Accept": "application/json",
+//       'Content-Type': 'application/json',
+//       'Authorization': 'Bearer wchufrIMyAPbjYd4ntbiDSK+WWaP4/AYZfVksIvfQrI=',
+//       'SecureURL': 'https://7206341744.3dcart.net/',
+//       'Token': 'wchufrIMyAPbjYd4ntbiDSK+WWaP4/AYZfVksIvfQrI='
+//     },
+//     credentials: 'same-origin',
+//     withCredentials : true
+//   }
+// window.onload = () => {
+//   console.log('loaded')
+//
+//   axios
+//     .get('https://apirest.3dcart.com/3dCartWebAPI/v1/Categories?limit=10&offset=1', settings)
+//     .then(response => {
+//       return response.json()
+//     })
+//     .then(result => {
+//       console.log(result)
+//     })
+//     .catch(err => console.log(err))
+// }
 
-  axios
-    .get(
-      'https://apirest.3dcart.com/3dCartWebAPI/v1/Customers?email=caglarergulcom%40gmail.com',
-      settings
-    )
-    .then(response => {
-      return response.json()
-    })
-    .then(result => {
-      console.log(result)
-    })
-    .catch(err => console.log(err))
-}
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("GET", "https://apirest.3dcart.com/3dCartWebAPI/v1/Products/7");
+xhr.setRequestHeader("Authorization", "Bearer wchufrIMyAPbjYd4ntbiDSK+WWaP4/AYZfVksIvfQrI=");
+
+xhr.send();
